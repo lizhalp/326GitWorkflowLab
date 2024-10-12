81ad753fbcdd568542c1fdfda432cea819509235
Kyle <hchua@umass.edu>
2024-10-12 06:01:44 -0400

    Implemented Natural Log Function

--- calculator.js
+++ calculator.js
@@ -1,51 +1,54 @@
-
-
 // TODO: Fix the square root function not displaying
-function calculateSquareRoot(value) {
-    
-    // return the square root of the value
+function calculateSquareRoot(value) { //Elizabeth
+  // return the square root of the value
 }
 
 // TODO: Implement square function
-function calculateSquare(value) {
-    // Hint: Use exponentiation
-    // return the square root of the value
-
+function calculateSquare(value) { // Megan
+  // Hint: Use exponentiation
+  // return the square root of the value
 }
 
-// BUG: FIX reciprocal function
-function calculateReciprocal(value) {
-    // Hint: Reciprocal is 1/x
-    // return the square root of the value
-    return 1* value;
-
+// TODO: FIX reciprocal function
+function calculateReciprocal() { // Timothy
+  // Hint: Reciprocal is 1/x
+  // return the square root of the value
+  return 1 * value;
 }
 
 // TODO: Implement natural log function
-function naturalLog(value) {
-    // return the natural log of the value
+function naturalLog(value) { // Kyle
+  if (value <= 0) return NaN;
+  let result = 0;
+  let term = (value - 1) / (value + 1);
+  let termSquared = term * term;
+  let numerator = term;
+
+  for (let i = 1; i < 1000; i += 2) {
+    result += numerator / i;
+    numerator *= termSquared;
+  }
+
+  return 2 * result;
 }
 
-// TODO: Implement sine function
+// TODO: Implement sine function // Alan
 function sine(value) {
-    // return the sine of the value
+  // return the sine of the value
 }
 
 // TODO: Implement cosine function
 function cosine(value) {
-    // return the cosine of the value
+  // return the cosine of the value
 }
 
-// TODO: Implement tangent function
+// TODO: Implement tangent function // Tuoshieng
 function tangent(value) {
-    // return the tangent of the value
+  // return the tangent of the value
 }
-
-
-
 
 module.exports = {
-    calculateSquareRoot,
-    calculateSquare,
-    calculateReciprocal
+  calculateSquareRoot,
+  calculateSquare,
+  calculateReciprocal,
 };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -13,6 +13,11 @@ describe('Calculator Calculation Functions', function() {
     it('should return 2', function() {
         assert.strictEqual(res, 2);
     });
+
+    it('naturalLog should return ~1.386 for input 4', function() {
+        const res = calculator.naturalLog(4);
+        assert.strictEqual(res.toFixed(3), '1.386');
+    });
 });
 
 
